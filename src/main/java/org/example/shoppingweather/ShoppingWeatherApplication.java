e66de0e6d69f6fd89c9668a1d5fbca5cacc9763c
Ninky0 <2020113392@dgu.ac.kr>
2024-10-08 17:04:34 +0900

    init:setting

---
+++ src/main/java/org/example/shoppingweather/ShoppingWeatherApplication.java
@@ -0,0 +1,13 @@
+package org.example.shoppingweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingWeatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingWeatherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/shoppingweather/ShoppingWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.shoppingweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingWeatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
